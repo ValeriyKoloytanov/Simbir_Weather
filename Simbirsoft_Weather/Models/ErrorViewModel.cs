56cebcc71b503d04d2c471af08cef1481e1b7dfd
Valeriy <igor.koloytanov@gmail.com>
2021-06-21 21:05:15 +0300

    initial

---
+++ Simbirsoft_Weather/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Simbirsoft_Weather.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
