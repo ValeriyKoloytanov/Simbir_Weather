03ee2555d189b228c4262a82a5c3ff9e70df97fc
Valeriy <igor.koloytanov@gmail.com>
2021-06-22 21:55:05 +0300

    initial api helper class

--- Simbirsoft_Weather/Program.cs
+++ Simbirsoft_Weather/Program.cs
@@ -6,13 +6,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Simbirsoft_Weather.Models;
 
 namespace Simbirsoft_Weather
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public   static void Main(string[] args)
         {
+            var test = new Wheather();
+            test.retrycount = 3;
+           var result=  test.Get_wheather("hjjkkl", "Budapest");
             CreateHostBuilder(args).Build().Run();
         }
 
